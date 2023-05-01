46df21c6e69ee204f5908896d7bc664a16c40c61
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-05-01 17:56:55 -0300

    feat: Created the configuration to mobile and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,36 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    maxWidth: {
+      container: "1440px",
+      contentContainer: "1140px",
+      containerSmall: "1024px",
+      containerxs: "768px",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+      fontFamily: {
+        bodyFont: ["Bruno Ace SC", "sans-serif"],
+        titleFont: ["Montserrat", "sans-serif"],
+      },
+      boxShadow: {
+          navBar: "0 10px 30px -10px rgba(2, 12, 27, 0.7)",
+      },
+      colors: {
+        bodyColor: "#0A192F",
+        textGreen: "#64ffda",
+        textLight: "#ccd6f6",
+        textDark: "#8892b0",
+        hoverColor: "rgba(100, 255, 218, 0.1)"
       },
     },
   },
