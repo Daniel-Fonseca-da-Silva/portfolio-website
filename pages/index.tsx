dd5be7339896bc402fcca5485e50e7f4e824359e
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-05-01 22:30:39 -0300

    feat: Created the navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main className="font-bodyFont">
-      New Portfolio Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores autem dolorum neque repudiandae suscipit? Quam, nulla molestiae iste voluptates est fugit eos sapiente impedit dolores perspiciatis culpa doloremque praesentium nesciunt.
+    <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
+      <Navbar />
     </main>
   )
 }
