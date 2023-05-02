11d8fb9ab9e955e1f080d5d2c2cb81a6ca27c5e4
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-05-01 23:59:55 -0300

    feat: Created the base to mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -88,6 +88,11 @@ const Navbar = () => {
           </motion.button>
          </a>
         </div>
+        {/* Mobile Navbar */}
+        <div className="w-6 h-5 flex flex-col justify-between items-center mdl:hidden text-4xl text-textGreen cursor-pointer overflow-hidden group">
+          <span className="w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300"></span>
+          <span className="w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-3 transition-all ease-in-out duration-300"></span><span className="w-full h-[2px] bg-textGreen inline-flex transform group-hover:translate-x-1 transition-all ease-in-out duration-300"></span>
+        </div>
       </div>
     </div>
   );
