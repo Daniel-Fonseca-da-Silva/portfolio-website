5df996e02827d75e1f7eb6940a1df48f02ba9d7c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-05-01 23:41:39 -0300

    feat: Added the framer-motion and implemented navbar to desktop

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import { logo } from "@/public/assets";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="w-full shadow-navBar h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
+      <div className="max-w-container h-full mx-auto py-1 font-titleFont flex items-center justify-between">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Image className="w-14" src={logo} alt="Logo Dafon" />
+        </motion.div>
+        <div className="hidden mdl:inline-flex items-center gap-7">
+          <ul className="flex text-[13px] gap-7">
+            <Link
+              href="#home"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                Home
+              </motion.li>
+            </Link>
+            <Link
+              href="#home"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.1 }}
+              >
+                <span>01.</span> About
+              </motion.li>
+            </Link>
+            <Link
+              href="#experience"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.2 }}
+              >
+                <span>02.</span> Experience
+              </motion.li>
+            </Link>
+            <Link
+              href="#project"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.3 }}
+              >
+                <span>03.</span> Projects
+              </motion.li>
+            </Link>
+            <Link
+              href="#contact"
+              className="flex items-center gap-1 font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.4 }}
+              >
+                <span>04.</span> Contact
+              </motion.li>
+            </Link>
+          </ul>
+         <a href="/assets/cv/developer.pdf" target="_blank">
+           <motion.button
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5 }}
+            className="px-4 py-2 rounded-md text-textGreen text-[13px] border border-textGreen hover:bg-hoverColor duration-300"
+          >
+            Resume
+          </motion.button>
+         </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
