51e28ffaaa8a1db98a02225c5e388a52851eaa52
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-05-09 00:55:27 -0300

    feat: Added another resources

--- components/About.tsx
+++ components/About.tsx
@@ -113,9 +113,9 @@ const About = () => {
                 alt="profileImg"
               />
               <div className="hidden lgl:inline-block absolute w-full h-80 bg-textGreen/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300"></div>
-              <div className="hidden lgl:inline-flex absolute w-full h-80 border-2 border-textGreen rounded-md group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300"></div>
             </div>
           </div>
+          <div className="hidden lgl:inline-flex absolute w-full h-80 border-2 border-textGreen rounded-md group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300"></div>
         </div>
       </div>
     </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/About";
 import Banner from "@/components/Banner";
+import Experience from "@/components/Experience";
 import LeftSide from "@/components/LeftSide";
 import Navbar from "@/components/Navbar";
 import RightSide from "@/components/RightSide";
@@ -29,7 +30,7 @@ export default function Home() {
           <div className="h-[88vh] w-full mx-auto p-4">
             <Banner />
             <About />
-            {/* Experience */}
+            <Experience />
             {/* Projects */}
             {/* Archive */}
             {/* Contact */}
@@ -39,7 +40,8 @@ export default function Home() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1.5 }}
-            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0">
+            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0"
+          >
             <RightSide />
           </motion.div>
         </div>
