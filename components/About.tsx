f1f92b9f9df8987f48d753b44fb27bbd440f3a30
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-05-26 15:04:32 -0300

    feat: Added correct description inside about

--- components/About.tsx
+++ components/About.tsx
@@ -14,33 +14,82 @@ const About = () => {
       <div className="flex flex-col lgl:flex-row gap-16">
         <div className="w-full lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-            earum corrupti quae molestiae,{" "}
-            <span className="text-textGreen">veniam quo rerum alias</span>
-            laboriosam ipsa at voluptates, illo et perferendis reprehenderit
-            necessitatibus tempore{" "}
-            <span className="text-textGreen">voluptatibus</span> excepturi
-            quasi?
+            Daniel Fonseca, I am a talented{" "}
+            <span className="text-textGreen"> software developer</span> with
+            five years of experience in the field. My expertise spans a variety
+            of technologies and frameworks, allowing me to work on both
+            <span className="text-textGreen">front-end and back-end</span>{" "}
+            application development. I have deep knowledge in Java, Spring,
+            NextJS, Typescript, Tailwind, Bootstrap, CSS3, HTML5, JavaScript,
+            NestJS, PostgreSQL, Redis, and MySQL.
           </p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-            earum corrupti quae molestiae,{" "}
-            <span className="text-textGreen">veniam quo rerum alias</span>
-            laboriosam ipsa at voluptates, illo et perferendis reprehenderit
-            necessitatibus tempore{" "}
-            <span className="text-textGreen">voluptatibus</span> excepturi
-            quasi?
+            In the realm of front-end development, I am{" "}
+            <span className="text-textGreen">highly skilled </span>in creating
+            attractive and functional user interfaces. I use technologies such
+            as HTML5, CSS3, and JavaScript to develop{" "}
+            <span className="text-textGreen">
+              responsive and user-friendly{" "}
+            </span>{" "}
+            web applications. My experience includes working with popular
+            frameworks like{" "}
+            <span className="text-textGreen">NextJS and Angular</span>, enabling
+            the creation of cutting-edge web applications. I am also proficient
+            in using{" "}
+            <span className="text-textGreen">Bootstrap or Tailwind </span>to
+            ensure a responsive and user-friendly design.
           </p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
-            earum corrupti quae molestiae,{" "}
-            <span className="text-textGreen">veniam quo rerum alias</span>
-            laboriosam ipsa at voluptates, illo et perferendis reprehenderit
-            necessitatibus tempore{" "}
-            <span className="text-textGreen">voluptatibus</span> excepturi
-            quasi?
+            When it comes to back-end development, I specialize in using
+            technologies like{" "}
+            <span className="text-textGreen">Java and Spring</span>. I am
+            capable of building robust APIs and scalable web applications using
+            these tools. Additionally, I have knowledge in{" "}
+            <span className="text-textGreen">NestJS, a Node.js framework</span>{" "}
+            for building efficient and well-structured server-side applications.
           </p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+          <p>
+            I also have a strong background in databases, including PostgreSQL,
+            Redis, and MySQL. I am skilled in{" "}
+            <span className="text-textGreen">
+              designing and modeling efficient databases
+            </span>
+            , ensuring proper data storage and retrieval for complex
+            applications.
+          </p>
+          <p>
+            In addition to my technical skills, I excel in supporting and
+            mentoring junior developers. I am always willing to{" "}
+            <span className="text-textGreen">share my knowledge</span> and help
+            the <span className="text-textGreen">team grow and develop</span>.
+            My experience includes working collaboratively, providing guidance,
+            and resolving{" "}
+            <span className="text-textGreen">technical challenges</span>.
+          </p>
+          <p>
+            Continuous improvement of front-end and back-end web systems is a
+            crucial aspect of my work. I constantly seek opportunities to{" "}
+            <span className="text-textGreen">
+              optimize and enhance performance , usability, and security{" "}
+            </span>
+            in the applications I develop. I stay updated with industry best
+            practices and trends, ensuring the systems I build align with the
+            highest quality standards.
+          </p>
+          <p>
+            In summary, I am an experienced and versatile software developer
+            with{" "}
+            <span className="text-textGreen">
+              strong skills in a variety of technologies and frameworks
+            </span>
+            . My experience spans both front-end and back-end development, and I
+            am highly efficient in creating{" "}
+            <span className="text-textGreen">
+              high-quality web applications
+            </span>
+            . With my passion for learning and knowledge sharing, I am a
+            valuable asset to any development team.
+          </p>
           <ul className="max-w-[450px] text-sm font-titleFont grid grid-cols-2 gap-2 mt-6">
             <li className="flex items-center gap-2">
               <span className="text-textGreen">
