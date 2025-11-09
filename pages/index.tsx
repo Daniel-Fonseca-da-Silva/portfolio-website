342a438e19a40be8554deda71dfc8cb600076b91
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-11-09 23:14:40 +0000

    fix: added scroll fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,9 +21,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="../public/assets/images/logo.png" />
       </Head>
-      <main className="w-full h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
+      <main className="w-full min-h-screen font-bodyFont bg-bodyColor text-textLight overflow-x-hidden scrollbar scrollbar-track-textDark/20 scrollbar-thumb-textDark/60">
         <Navbar />
-        <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+        <div className="w-full xl:flex items-center gap-20 justify-between">
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
@@ -32,7 +32,7 @@ export default function Home() {
           >
             <LeftSide />
           </motion.div>
-          <div className="h-[88vh] w-full mx-auto p-4">
+          <div className="w-full mx-auto p-4">
             <Banner />
             <About />
             <Experience />
