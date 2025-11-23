f23d5525de3c1f170d8df51a894ad06bbe0665ac
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2025-11-23 01:07:50 +0000

    feat: added logo to be favicon icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/assets/images/logo.png" />
+        <link rel="shortcut icon" href="/assets/images/logo.png" />
+        <link rel="apple-touch-icon" href="/assets/images/logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
